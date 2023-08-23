7336d05e613a1e12b549c97e78e825db4777c2d9
Bruno Fernando <bfernandodco@protonmail.com>
2023-08-23 00:40:41 -0300

    Leitura de dados no console com Scanner.

---
+++ primeiroProjeto/src/leituraDeDados/Scanear.java
@@ -0,0 +1,24 @@
+package leituraDeDados;
+
+import java.util.Scanner;
+
+public class Scanear {
+
+	public static void main(String[] args) {
+		/*INSTANCIANDO UM OBJETO SCANNER COM O PARAMETRO System.in QUE REALIZA LEITURAS DO TECLADO
+		DIFERENTE DO JOPTIONPANE, O Scanner SOLICITA A ENTRADA DE DADOS NO CONSOLE
+		*/
+		Scanner leitura = new Scanner(System.in);
+		
+		System.out.println("Digite seu filme favorito");
+		String filme = leitura.nextLine();
+		System.out.println(filme);
+		System.out.println("Ano de lançamento");
+		String ano = leitura.nextLine();
+		System.out.println(ano);
+		System.out.println("Qual é a nota que você dá para o filme?");
+		//INFORME A NOTA CONFORME O IDIOMA DO TECLADO LOCAL (EX: PT-BR = 9,9)
+		double nota = leitura.nextDouble();
+		System.out.println(nota);
+	}
+}
