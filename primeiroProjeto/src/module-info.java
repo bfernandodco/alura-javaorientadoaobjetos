76ff776bfdfb91a999740e4b658cec1a08b85d7e
Bruno Fernando <bfernandodco@protonmail.com>
2023-08-22 23:53:06 -0300

    Software de conversão de temperatura. Celsius para Fahrenheit.

--- primeiroProjeto/src/module-info.java
+++ primeiroProjeto/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module primeiroProjeto {
+	requires java.desktop;
 }
---
+++ primeiroProjeto/src/primeiroProjeto/ConversorTemperatura.java
@@ -0,0 +1,19 @@
+package primeiroProjeto;
+
+import javax.swing.JOptionPane;
+
+public class ConversorTemperatura {
+
+	public static void main(String[] args) {
+		
+		//ENTRADA DE DADOS DA TEMPERATURA
+		String celsius = JOptionPane.showInputDialog("Informe a temperatura em Celsius");
+		
+		//CONVERSÃO DE STRING PARA DOUBLE
+		double grausCelsius = Double.parseDouble(celsius);
+		
+		//CÁLCULO DE CONVERSÃO
+		double fahrenheit = (grausCelsius * 1.8) + 32;
+		System.out.printf("A temperatura de %s°C em Fahrenheit é de: %.0f°F",celsius,fahrenheit);
+	}
+}
