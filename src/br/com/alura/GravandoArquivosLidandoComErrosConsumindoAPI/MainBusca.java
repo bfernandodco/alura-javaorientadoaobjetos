1b8ddff62e463c7ee38be419dd6c82c0dc51282b
bfernandodco <bfernandodco@protonmail.com>
2023-08-29 06:10:11 -0300

    API OMDb foi integrada.

--- src/br/com/alura/GravandoArquivosLidandoComErrosConsumindoAPI/MainBusca.java
+++ src/br/com/alura/GravandoArquivosLidandoComErrosConsumindoAPI/MainBusca.java
@@ -5,19 +5,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class MainBusca {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
+		Scanner leitura = new Scanner(System.in);
+		System.out.println("Digite um filme para a busca");
+		var filme = leitura.nextLine();
+		String endereco = "http://www.omdbapi.com/?t=" + filme + "&apikey=fd9f2b16";
 		
-	HttpClient client = HttpClient.newHttpClient();
-	HttpRequest request = HttpRequest.newBuilder()
-	.uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=fd9f2b16"))
-	//DESIGN PATTERN
-	.build();
-	 HttpResponse<String> response = client
-			 .send(request, HttpResponse.BodyHandlers.ofString());
-	 System.out.println(response.body());
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+		.uri(URI.create(endereco))
+		//DESIGN PATTERN
+		.build();
+		 HttpResponse<String> response = client
+				 .send(request, HttpResponse.BodyHandlers.ofString());
+		 System.out.println(response.body());
 	
 	}
 }
