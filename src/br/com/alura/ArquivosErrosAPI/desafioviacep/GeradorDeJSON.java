da1e61395868f2897a296062cc744251cc87544a
bfernandodco <bfernandodco@protonmail.com>
2023-08-29 20:30:34 -0300

    Gerado o arquivo JSON contendo o endereço conforme o cep informado.

--- src/br/com/alura/ArquivosErrosAPI/desafioviacep/GeradorDeJSON.java
+++ src/br/com/alura/ArquivosErrosAPI/desafioviacep/GeradorDeJSON.java
@@ -11,7 +11,7 @@ public class GeradorDeJSON {
 	public void salvaJson(Endereco endereco) throws IOException {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-		FileWriter escrita = new FileWriter(endereco.cep());
+		FileWriter escrita = new FileWriter(endereco.cep() + ".json");
 		escrita.write(gson.toJson(endereco));
 		escrita.close();
 	}
